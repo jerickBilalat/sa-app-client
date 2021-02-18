da8b7c833670af1361d74359015dc30cdbed0478
Jerick Bilalat <jrk.email@yahoo.com>
2021-02-18 10:04:43 -0600

    ft started app component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import * as React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import axios from 'axios'
+import {UnauthenticatedApp} from './unauthenticated-app'
+import {AuthenticatedApp} from './authenticated-app'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn CI
-        </a>
-      </header>
-    </div>
+  const [user, setUser] = React.useState(null)
+  React.useEffect(() => {
+    function fetchUser() {
+      const result = axios.get(
+        'http://localhost:8080/api/auth/get-user-current-data',
+        {
+          method: 'GET',
+          headers: {
+            'x-auth-token':
+              'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2MDJlODNmOTgyYTA4NzhhMWFlYTMxZmMiLCJpYXQiOjE2MTM2NjExNzgsInVzZXJuYW1lIjoidGVzdHVzZXIiLCJpZCI6IjYwMmU4M2Y5ODJhMDg3OGExYWVhMzFmYyJ9.5m-1CceQkhwU2QcpOxSGsXJRHEYm9JsrcdVjen-fGEg',
+          },
+        },
+      )
+      return result
+    }
+    fetchUser().then(res => {
+      setUser(res)
+    })
+  }, [])
+
+  const props = {user}
+  return user ? (
+    <AuthenticatedApp {...props} />
+  ) : (
+    <UnauthenticatedApp {...props} />
   )
 }
 
-export default App
+export {App}
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,92 @@
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import App from './App'
+import {createContainer} from './testsUtils/domManipulators'
+import {App} from './App'
+import * as UnauthenticatedAppExports from './unauthenticated-app'
+import * as AuthenticatedAppExports from './authenticated-app'
+import axios from 'axios'
 
-test('renders learn react link', () => {
+test('fetch user data after mounting', async () => {
+  let {renderAndWait} = createContainer()
+
+  jest.spyOn(axios, 'get').mockImplementation(() =>
+    Promise.resolve({
+      status: 'ok',
+    }),
+  )
+
+  await renderAndWait(<App />)
+
+  expect(axios.get).toHaveBeenCalledWith(
+    'http://localhost:8080/api/auth/get-user-current-data',
+    expect.objectContaining({
+      method: 'GET',
+      headers: {'x-auth-token': expect.anything()},
+    }),
+  )
+
+  axios.get.mockRestore()
+})
+
+test('render unauthenticated app initialy when user data is not fetched', () => {
+  let {render} = createContainer()
+
+  jest
+    .spyOn(UnauthenticatedAppExports, 'UnauthenticatedApp')
+    .mockReturnValue(null)
+
   render(<App />)
-  const linkElement = screen.getByText(/learn ci/i)
-  expect(linkElement).toBeInTheDocument()
+
+  expect(UnauthenticatedAppExports.UnauthenticatedApp).toHaveBeenCalled()
+
+  UnauthenticatedAppExports.UnauthenticatedApp.mockRestore()
+})
+
+test('render authenticated app when user data is fetched', async () => {
+  let {renderAndWait} = createContainer()
+
+  jest.spyOn(AuthenticatedAppExports, 'AuthenticatedApp').mockReturnValue(null)
+  jest.spyOn(axios, 'get').mockImplementation(() =>
+    Promise.resolve({
+      user: 'user',
+    }),
+  )
+  await renderAndWait(<App />)
+
+  expect(AuthenticatedAppExports.AuthenticatedApp).toHaveBeenCalled()
+
+  AuthenticatedAppExports.AuthenticatedApp.mockRestore()
+  axios.get.mockRestore()
+})
+
+test('pass collected props to children', async () => {
+  let {renderAndWait} = createContainer()
+
+  jest.spyOn(AuthenticatedAppExports, 'AuthenticatedApp').mockReturnValue(null)
+  jest
+    .spyOn(UnauthenticatedAppExports, 'UnauthenticatedApp')
+    .mockReturnValue(null)
+  jest.spyOn(axios, 'get').mockImplementation(() =>
+    Promise.resolve({
+      testprop: '123',
+    }),
+  )
+  await renderAndWait(<App />)
+
+  expect(AuthenticatedAppExports.AuthenticatedApp).toHaveBeenCalledWith(
+    {
+      user: {testprop: '123'},
+    },
+    expect.anything(), // seems like not only props are passed to the function when evoked
+  )
+
+  expect(UnauthenticatedAppExports.UnauthenticatedApp).toHaveBeenCalledWith(
+    {
+      user: null,
+    },
+    expect.anything(), // seems like not only props are passed to the function when evoked
+  )
+
+  AuthenticatedAppExports.AuthenticatedApp.mockRestore()
+  UnauthenticatedAppExports.UnauthenticatedApp.mockRestore()
+  axios.get.mockRestore()
 })
